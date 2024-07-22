b794a45641e375cc13a07e78a77c8f55fa683705
Jana Plumm <janaplumm@outlook.com>
2024-07-22 11:57:15 +1000

    collapsibles

--- scripts.js
+++ scripts.js
@@ -222,3 +222,16 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
+
+// COLLAPSIBLES
+document.querySelectorAll('.collapsible-button').forEach(button => {
+  button.addEventListener('click', () => {
+      button.classList.toggle('active');
+      const content = button.nextElementSibling;
+      if (button.classList.contains('active')) {
+          content.style.maxHeight = content.scrollHeight + 'px';
+      } else {
+          content.style.maxHeight = 0;
+      }
+  });
+});
